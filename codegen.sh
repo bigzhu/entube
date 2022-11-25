3ff93ef4344f38201e32e0bcb1cb3a28593c419b
bigzhu <vermiliondun@gmail.com>
2022-11-25 09:30:17 +0800

    generate the code next to the .graphql files

--- codegen.sh
+++ codegen.sh
@@ -1,4 +1,4 @@
 source .env
-gq https://jysijxgffjwavdtqcuir.nhost.run/v1/graphql -H "x-hasura-admin-secret: $HASURA_GRAPHQL_ADMIN_SECRET" --introspect > lib/schema.graphql
+gq https://jysijxgffjwavdtqcuir.nhost.run/v1/graphql -H "x-hasura-admin-secret: $HASURA_GRAPHQL_ADMIN_SECRET" --introspect > lib/graphql/schema.graphql
 #npm run flutter-freezed
 flutter pub run build_runner build --delete-conflicting-outputs
