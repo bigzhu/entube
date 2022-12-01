e30e7c413c36b0083c4b128b711d63f70573e046
bigzhu <vermiliondun@gmail.com>
2022-12-01 17:06:00 +0800

    gencode must in same path

--- lib/components/AcquiringWords/state.dart
+++ lib/components/AcquiringWords/state.dart
@@ -1,9 +1,9 @@
+import 'package:entube/components/AcquiringWords/g/services.req.gql.dart';
+import 'package:entube/state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 final acquiringWordsStreamP = Provider((ref) {
-  final articlesReq = GArticleItemsReq(
-    (b) => b
-      ..vars.limit = 10
-      ..vars.offset = 0,
-  );
+  final articlesReq = GAcquiringWordsReq();
   final client = ref.watch(gqlClientP);
   return client.request(articlesReq);
 });
