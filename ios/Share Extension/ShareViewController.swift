7adb3c107927c1ca17d41d7373ca7e643e9444a9
bigzhu <vermiliondun@gmail.com>
2022-12-17 16:22:29 +0800

    update  ios/Runner/Info.plist to make receive_sharing_intent work

--- ios/Share Extension/ShareViewController.swift
+++ ios/Share Extension/ShareViewController.swift
@@ -4,7 +4,7 @@ import MobileCoreServices
 import Photos
 
 class ShareViewController: SLComposeServiceViewController {
-    var hostAppBundleIdentifier = "net.bigzhu.acquireEnglish"
+    var hostAppBundleIdentifier = ""
     var appGroupId = ""
     let sharedKey = "ShareKey"
     var sharedMedia: [SharedMediaFile] = []
